a12e7bdb2697b6b8662882ecda76d96d665802ff
nafikfirmansyah83 <nafikfirmansyah83@gmail.com>
2025-02-28 11:10:45 +0700

    edit description

--- logout.php
+++ logout.php
@@ -1,7 +1,7 @@
 <?php
     session_start();
     unset($_SESSION['email']);
-    session_destroy(); // berfungsi untuk menutup semua session
+    session_destroy();
     
     echo '<script language="javascript">alert("Anda berhasil Logout!"); document.location="index.php";</script>'; // memunculkan peringatan kemudian me-redirect ke halaman depan
 ?>
